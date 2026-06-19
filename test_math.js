34c8825cd9938070348caac2700d377b93252caf
Manikandan <nrgmanikandan91@gmail.com>
2026-06-19 19:56:16 +0530

    removed test doc

--- test_math.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const svg = fs.readFileSync('test_output.svg', 'utf8');
-const match = svg.match(/<path[^>]*marker-start="url\(#cutonfoldFrom\)"[^>]*>/g);
-console.log(match);
